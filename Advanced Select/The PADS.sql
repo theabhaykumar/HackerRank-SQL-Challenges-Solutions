12efc77364855608376dedc409e96e165ac972e2
Abhay Kumar <74042989+theabhaykumar@users.noreply.github.com>
2024-01-13 01:06:55 +0530

    Update The PADS.sql

--- Advanced Select/The PADS.sql
+++ Advanced Select/The PADS.sql
@@ -1,8 +1,5 @@
 # Author: Thomas George Thomas
-select concat(name,'(',substring(Occupation,1,1),')') as Name 
-from occupations 
-order by Name;
-Select concat ('There are a total of ', count(occupation),' ', lower(occupation),'s.') as totals
-from occupations
-group by occupation
-order by totals
+SELECT NAME || '(' || SUBSTR(OCCUPATION,1,1)||')' FROM OCCUPATIONS
+UNION
+SELECT 'There are a total of '|| count(occupation) || ' ' || LOWER(occupation)||'s.' FROM OCCUPATIONS
+GROUP BY OCCUPATION;
